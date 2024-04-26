d06f562d3366a027de2a3a187d291bfccd6a8ab7
LRN251 <150847992+LRN251@users.noreply.github.com>
2024-04-26 19:10:39 +0000

    First commit  feature/endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -3,11 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __table_name__='user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    nickname = db.column(db.String(40),unique=False, nullable=False)
+    name = db.column(db.String(40),unique=False, nullable=False)
+    lastname = db.column(db.String(40),unique=False, nullable=False)
+    rol = db.column(db.String(40),unique=False, nullable=False)
+    calendar = db.column(db.DateTime,unique=False, nullable=True)
     def __repr__(self):
         return f'<User {self.email}>'
 
@@ -15,5 +19,38 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "nickname":self.nickname,
+            "name":self.name,
+            "lastname":self.lastname,
+            "rol":self.rol,
+            "calendar":self.calendar
+        }
+    
+    class Coach(db.Model):
+        __table_name__='coach'
+        id = db.Column(db.Integer, primary_key=True)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        password = db.Column(db.String(80), unique=False, nullable=False)
+        nickname = db.column(db.String(40),unique=False, nullable=False)
+        name = db.column(db.String(40),unique=False, nullable=False)
+        lastname = db.column(db.String(40),unique=False, nullable=False)
+        description = db.Column(db.String(180), unique=False, nullable=False)
+        image = db.Column(db.String(180), unique=False, nullable=True)
+        speciality = db.Column(db.String(80), unique=False, nullable=False)
+        calendar = db.column(db.DateTime,unique=False, nullable=True)
+
+    def __repr__(self):
+        return f'<Coach {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "nickname":self.nickname,
+            "name":self.name,
+            "lastname":self.lastname,
+            "description":self.description,
+            "image":self.image,
+            "speciality": self.speciality,
+            "calendar":self.calendar
         }
