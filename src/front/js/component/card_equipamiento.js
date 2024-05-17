6fd92e7e92a79456e86566cbf3fd626c381e0b60
Steven Rojas <48265453+StevenRs12@users.noreply.github.com>
2024-05-17 19:48:07 +0000

    modify webpack

--- src/front/js/component/card_equipamiento.js
+++ src/front/js/component/card_equipamiento.js
@@ -3,7 +3,7 @@ import "../../styles/home.css";
 /* import foto_producto from "../../img/equipamiento/Banda_de-_resistencia.jpg"; */
 import { Link } from "react-router-dom";
 
-const ProductCardEquipamiento = ({ product}) => {
+const ProductCardEquipamiento = ({ product }) => {
   const cardRef = React.useRef();
 
   if (product.type !== "Equipamiento") {
@@ -13,7 +13,8 @@ const ProductCardEquipamiento = ({ product}) => {
   return (
     <Link to={`/product/${product.id}`} ref={cardRef}>
       <div class="card" style={{ width: "15rem" }}>
-        <img src={product.image_product} className="card-img-top" alt={product.product} />
+        <img src={`images/${product.image_product}`} // Ruta relativa a 'dist/images'
+          className="card-img-top" alt={product.product} />
         <div class="card-body">
           <h5 className="card-title">{product.product}</h5>
           <p className="card-text">{product.price}</p>
