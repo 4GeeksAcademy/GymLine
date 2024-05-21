dee34c21406d0150b07a583547237b8a2a0f2e28
Nanit20 <55482552+Nanit20@users.noreply.github.com>
2024-05-21 19:29:14 +0100

    Update adminview.js

--- src/front/js/pages/adminview.js
+++ src/front/js/pages/adminview.js
@@ -3,23 +3,21 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 const Adminview = () => {
-    
     const { store, actions } = useContext(Context);
 
     useEffect(() => {
-        
         if (!store.logged) {
             actions.verifyAuthToken();
         }
     }, [store.logged]);
 
     return (
-        <div className="text-center">
+        <div className="text-center adminview-container">
             {store.logged && store.user && store.user.rol === "admin" ? (
                 <div>
-                    <h1>Bienvenido {store.user.name}, ¿Qué deseas revisar hoy?</h1>
+                    <h1 className="welcome-message">Bienvenido {store.user.name}, ¿Qué deseas revisar hoy?</h1>
                 </div>
-            ) : store.logged == false ? (
+            ) : store.logged === false ? (
                 <div>
                     <h1>Authenticating</h1>
                     <p>Checking..................</p>
@@ -31,17 +29,22 @@ const Adminview = () => {
                 </div>
             )}
             {store.logged && store.user && store.user.rol === "admin" && (
-                <div className="button-container-adminview">
-                    <Link to="/shopmanagment">
-                        <button className="button-adminview">Tienda</button>
-                    </Link>
-                    <Link to="/usersmanagment">
-                        <button className="button-adminview">Usuarios</button>
-                    </Link>
-                    <Link to="/gymmanagment">
-                        <button className="button-adminview">Gimnasio</button>
-                    </Link>
-             </div>
+                <div className="sections-container">
+                    <div className="section">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0FFcddxkKtOuaw917QpUl64FlusvDaPIVTyCmh9yCVA&s" alt="Tienda" className="section-image" />
+                        <p className="section-text">Gestiona tu tienda de manera eficiente y rápida.</p>
+                        <Link to="/shopmanagment">
+                            <button className="section-button button_slide slide_left">Tienda</button>
+                        </Link>
+                    </div>
+                    <div className="section">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBNNHH2D3x_Vr1RfGdiq2KKIzf13R9qmncJg&s" alt="Usuarios" className="section-image" />
+                        <p className="section-text">Administra tus usuarios y mantén todo en orden.</p>
+                        <Link to="/usersmanagment">
+                            <button className="section-button button_slide slide_left">Usuarios</button>
+                        </Link>
+                    </div>
+                </div>
             )}
         </div>
     );
